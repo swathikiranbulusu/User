459e33881d718421eeda0601c662b7786768c1d9
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-07 20:36:20 +0530

    Update userRoutes.js

--- user-service/routes/userRoutes.js
+++ user-service/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const router = express.Router();
 const { register, login, profile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
-const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', register); // ✅ this defines /api/users/register
 router.post('/login', login);
 router.get('/profile', auth, profile);
 
