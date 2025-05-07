401783cdf26d33383e4e663b1a3e5ba836263351
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 00:42:24 +0530

    Update userController.js

--- user-service/controllers/userController.js
+++ user-service/controllers/userController.js
@@ -19,3 +19,13 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password'); // hide passwords
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
