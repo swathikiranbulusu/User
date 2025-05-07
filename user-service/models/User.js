cc2f7da8dcff7c82fb6b8056fb9550caaab9af3e
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 00:29:34 +0530

    Update2 User.js

--- user-service/models/User.js
+++ user-service/models/User.js
@@ -4,25 +4,33 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: [true, 'Name is required']
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email is required'],
     unique: true
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Password is required']
   }
 });
 
-
+// Hash password before saving
 userSchema.pre('save', async function (next) {
+  // Skip if password is not being modified
   if (!this.isModified('password') || !this.password) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
 
+  console.log('📛 Password before hashing:', this.password); // Debug log
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = mongoose.model('User', userSchema);
