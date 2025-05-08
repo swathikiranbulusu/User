1e295d21207688ee0c48811ba31cc851e88d656d
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 17:21:34 +0530

    Updateback userRoutes.js

--- user-service/routes/userRoutes.js
+++ user-service/routes/userRoutes.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, profile, getAllUsers, getUserById } = require('../controllers/userController');
+const { register, login, profile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
-
-
-router.get('/', (req, res) => {
-  console.log('📡 /api/users route hit directly');
-  res.send('User list route working ✅');
-});
-          // GET /api/users
-router.get('/:id', getUserById);         // GET /api/users/:id
 
 router.post('/register', register);      // POST /api/users/register
 router.post('/login', login);            // POST /api/users/login
